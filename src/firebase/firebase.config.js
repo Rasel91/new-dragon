5910a3dd270aff08199c2b6144c4bc3df7a96cbf
Rasel91 <srasel.dpe@gmail.com>
2022-12-16 09:18:37 +0600

    first commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_apiKey,
+//   authDomain: process.env.REACT_APP_authDomain,
+//   projectId: process.env.REACT_APP_projectId,
+//   storageBucket: process.env.REACT_APP_storageBucket,
+//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//   appId: process.env.REACT_APP_appId,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,14 +23,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCMhXcPeuQIDfy1rLMRMcAlYK5s8rxlQBU",
+  authDomain: "my-first-app-7b9bd.firebaseapp.com",
+  projectId: "my-first-app-7b9bd",
+  storageBucket: "my-first-app-7b9bd.appspot.com",
+  messagingSenderId: "600630982125",
+  appId: "1:600630982125:web:46226e2d64a4fbba7063e4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
 export default app;
